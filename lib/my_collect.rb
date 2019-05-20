0a80b7c7b084b2f3be24f0883ebaf6c4f53866ee
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-20 22:55:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(arg)
   counter = 0
   return_arg = []
   while counter < arg.length
-    new_ = yield arg[counter]
+     yield arg[counter]
     return_arg << new_
   end
   return_arg
